d21ba5dbdc6765f681a94737a5fe50c9a53422e2
Pei Zhang <pezhang@thoughtworks.com>
2018-03-13 11:30:35 +0800

    modify script shell

--- script.sh
+++ script.sh
@@ -1,34 +1,34 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 master_branch=$(git branch | grep master)
 test_branch=$(git branch | grep test)
 
 if [[ ! -n $master_branch  &&  ! -n $master_branch ]]; then
-    echo "分支不存在!"
+    echo "分支不存在!";
     exit 1;
 fi
 
 git checkout master
 if [ ! -f master.txt ]; then
-    echo "master.txt不存在!"
+    echo "master.txt不存在!";
     exit 1;
 fi
 
 master_commit_count=$(git rev-list --count master)
 
 if [ $master_commit_count  -lt 2 ]; then
-    echo "master commit数量不足2!"
+    echo "master commit数量不足2!";
     exit 1;
 fi
 
 git checkout test
 if [ ! -f test.txt ]; then
-    echo "test.txt不存在!"
+    echo "test.txt不存在!";
     exit 1;
 fi
 
 test_commit_count=$(git rev-list --count test)
 
 if [ $test_commit_count  -lt 2 ]; then
-    echo "test commit数量不足2!"
+    echo "test commit数量不足2!";
     exit 1;
 fi
